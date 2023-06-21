55ae89440066ae248f1e428f6000936baef62e1a
gpetersonrust <gpeterson@rustcollege.edu>
2023-06-21 10:09:56 -0500

    Remove Old Tailwind

--- update-tailwind.js
+++ update-tailwind.js
@@ -2,61 +2,67 @@ const fs = require('fs');
 const find_files_recursively = require('../utilities/FS/find_files_recursively');
 
  
- function buildTailwindConfig() {
-   let dirname = process.cwd(); 
+function buildTailwindConfig(tailwindConfig) {
+  const dirname = process.cwd();
+  const themes_directory = dirname + '/themes';
 
-const configurations  =  {
-    myComponents: getStyles(find_files_recursively(
-    dirname +  '/components',
+  //  find all json files in the themes directory
+  const files = find_files_recursively(
+    themes_directory,
     (file_list = []),
     (excludes = 'node_modules|dist|.git'),
     (file_type = '.json$')
-    )),
-    myBase: getStyles(find_files_recursively(
-    dirname +  '/base',
-    (file_list = []),
-    (excludes = 'node_modules|dist|.git'),
-    (file_type = '.json$')
-    )),
-    myUtilities: getStyles(find_files_recursively(
-    dirname +  '/utilities',
-    (file_list = []),
-    (excludes = 'node_modules|dist|.git'),
-    (file_type = '.json$')
-    ))
-}
- function getStyles(files) {
-  const styles = {};
+  );
+
+  let components = {};
+  let base = {};
+  let utilities = {};
 
   for (const file of files) {
     // read the file
     const text = fs.readFileSync(file, 'utf8');
     const json = JSON.parse(text);
- // merge the styles from the file into the styles object
-    Object.assign(styles, ...json);
+    //  if file name contains components
+    if (file.includes('components')) {
+      // merge the styles from the file into the styles object
+      Object.assign(components, ...json);
+    }
+    //  if file name contains base
+    if (file.includes('base')) {
+      // merge the styles from the file into the styles object
+      Object.assign(base, ...json);
+    }
+    //  if file name contains utilities
+    if (file.includes('utilities')) {
+      // merge the styles from the file into the styles object
+      Object.assign(utilities, ...json);
+    }
   }
- return styles;
-}
-   let text = '';
+  const configurations = {
+    components,
+    base,
+    utilities,
+  };
+  let text = '';
   for (const key in configurations) {
-  
-    // i need text to add on const var ${key} = ${JSON.stringify(configurations[key])};
-    text += `const ${key} = ${JSON.stringify(configurations[key])};\n`;
-      
+    text += `\n const ${key} = ${JSON.stringify(configurations[key])};`;
 
+    //  add line breaks after  ,
+    text = text.replace(/},/g, '},\n');
   }
+  text +=  `\n module.exports = {
+    components,
+    base,
+    utilities,
+  };`;
 
-  text += `module.exports = {
-    myUtilities,
-    myComponents,
-    myBase
-};`;
-     fs.writeFileSync(dirname + '/app/tailwind-plugin.js', text  );
- 
-}
- 
+  // where there are more than one \n replace with one
+  text = text.replace(/\n+/g, '\n');
+  
+
+  fs.writeFileSync(dirname + '/app/tailwind-plugin.js', text);
+
+};
+
 buildTailwindConfig();
-
-console.log('====================================');
-console.log('chase tailwind config updated');
-console.log('====================================');
+  
